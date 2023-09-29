c49240cf7b34eac6a91c20eaefa734614ba3c61c
Mauro de Oliveira André <mauroolivere@hotmail.com>
2023-09-29 14:44:36 +0100

    Add enum Weekdays, and button

---
+++ Calendar/Calendar/ButtonNavigationMonths.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonNavigationMonths.swift
+//  Calendar
+//
+//  Created by Mauro de Oliveira André on 28/09/2023.
+//
+
+import SwiftUI
+
+struct ButtonNavigationMonths: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Image(systemName:"arrowshape.backward.circle")
+                .font(.title)
+                .foregroundStyle(.black)
+        })
+    }
+}
+
+#Preview {
+    ButtonNavigationMonths()
+}
+
+
+//: arrowshape.right.circle
---
+++ Calendar/Calendar/Weekdays.swift
@@ -0,0 +1,21 @@
+//
+//  Weekdays.swift
+//  Calendar
+//
+//  Created by Mauro de Oliveira André on 29/09/2023.
+//
+
+import Foundation
+
+
+
+
+enum Weekdays{
+    case Seg
+    case Ter
+    case Qua
+    case Qui
+    case Sex
+    case Sáb
+    case Dom
+}
